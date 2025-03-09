e0d1d366c58770ce3c7964d85479e6f9a5a7cf4c
Simon Fuchs <fuchs.simon@gmail.com>
2025-03-09 12:51:32 +0100

    notepad video 02

---
+++ script.js
@@ -0,0 +1,15 @@
+let notes = ['banana', 'rasen mähen'];
+
+function renderNotes() {
+
+    let contentRef = document.getElementById('content');
+
+    contentRef.innerHTML = notes;
+}
+
+
+
+
+// notizen hinzufügen
+// notizen löschen
+// notiven archivieren
