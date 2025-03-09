a810a045a09f1fb2dfc799286124636d0bbd2ad7
Simon Fuchs <fuchs.simon@gmail.com>
2025-03-09 15:00:45 +0100

    notepad video 05

--- script.js
+++ script.js
@@ -7,17 +7,51 @@ function renderNotes() {
     contentRef.innerHTML = "";
 
     for (let indexNote = 0; indexNote < notes.length; indexNote++) {
-        const note = notes[indexNote];
-        contentRef.innerHTML += getNotesHtmlTemplate(indexNote);
+        contentRef.innerHTML += getNotesTemplate(indexNote);
     }
 }
 
-function getNotesHtmlTemplate(indexNote) {
+function getNotesTemplate(indexNote) {
     return /*html*/`
-        <p>+ ${notes[indexNote]}</p>`       // styling
+        <p id="${indexNote}" onclick="selectNote(${indexNote})">+ ${notes[indexNote]} 
+            <button onclick="deleteNote(${indexNote})">X</button>
+        </p>
+
+    `      
 }
 
+function addNote() {
+    let noteInputRef = document.getElementById('note-input');
+    let noteInput = noteInputRef.value;
 
-// notizen hinzufügen
-// notizen löschen
+    notes.push(noteInput);
+
+    renderNotes();
+
+    noteInputRef.value = "";
+}
+
+function deleteNote(indexNote) {
+    notes.splice(indexNote, 1);
+    renderNotes();
+}
+// für Auswahl zum löschen:
+// function selectNote(indexNote){
+//     let clickedNote = document.getElementById(indexNote);
+//     deselectNote();
+//     clickedNote.classList.add("note-selected");
+// }
+
+// function deselectNote(){
+//     for (let i = 0; i < notes.length; i++) {
+//         const note = document.getElementById([i]);
+//         note.classList.remove('note-selected');        
+//     }
+// }
+
+
+
+
+
+
 // notiven archivieren
